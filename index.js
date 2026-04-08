bc195102a38c2394bdb182f7aaf4d1ce19e882f0
blackperson12121 <SerialDesignationLX@outlook.com>
2026-04-08 03:28:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -11,28 +11,27 @@ const client = new DJS.Client({
   partials: [DJS.Partials.Channel],
 });
 
-const BOT_TOKEN = process.env.BOT_TOKEN;
-
 client.once('ready', function() {
   console.log('Online as ' + client.user.tag);
 });
 
-client.on('messageCreate', function(message) {
-  if (message.author.bot) return;
-  var c = message.content.trim();
-  var ch = message.channel;
-  var isMod = [message.me](https://message.me)mber && [message.me](https://message.me)mber.permissions.has('ModerateMembers');
+client.on('messageCreate', function(msg) {
+  if (msg.author.bot) return;
+  var c = msg.content.trim();
+  var ch = msg.channel;
+  var mbr = [msg.me](https://msg.me)mber;
+  var isMod = mbr && mbr.permissions.has('ModerateMembers');
 
   if (c === '!ping') { ch.send('Pong.'); return; }
   if (c === '!help') { ch.send('Commands: !ping !help !info !rules !warn !clear !kick !ban'); return; }
-  if (c === '!info') { ch.send('Server: ' + message.guild.name + ' | Members: ' + [message.guild.me](https://message.guild.me)mberCount); return; }
+  if (c === '!info') { ch.send('Server: ' + msg.guild.name + ' | Members: ' + [msg.guild.me](https://msg.guild.me)mberCount); return; }
   if (c === '!rules') { ch.send('1. Be respectful.\n2. No spam.\n3. Follow Discord ToS.'); return; }
 
   if (c.startsWith('!warn')) {
     if (!isMod) { ch.send('No permission.'); return; }
-    var u = [message.me](https://message.me)ntions.users.first();
-    if (!u) { ch.send('Usage: !warn @user reason'); return; }
-    ch.send('Warning issued to ' + u.username);
+    var wu = [msg.me](https://msg.me)ntions.users.first();
+    if (!wu) { ch.send('Usage: !warn @user reason'); return; }
+    ch.send('Warning issued to ' + wu.username);
     return;
   }
 
@@ -46,7 +45,7 @@ client.on('messageCreate', function(message) {
 
   if (c.startsWith('!kick')) {
     if (!isMod) { ch.send('No permission.'); return; }
-    var km = [message.mentions.me](https://message.mentions.me)mbers.first();
+    var km = [msg.mentions.me](https://msg.mentions.me)mbers.first();
     if (!km) { ch.send('Usage: !kick @user'); return; }
     km.kick().then(function() { ch.send('Kicked.'); }).catch(function() { ch.send('Failed.'); });
     return;
@@ -54,11 +53,11 @@ client.on('messageCreate', function(message) {
 
   if (c.startsWith('!ban')) {
     if (!isMod) { ch.send('No permission.'); return; }
-    var bm = [message.mentions.me](https://message.mentions.me)mbers.first();
+    var bm = [msg.mentions.me](https://msg.mentions.me)mbers.first();
     if (!bm) { ch.send('Usage: !ban @user'); return; }
     bm.ban().then(function() { ch.send('Banned.'); }).catch(function() { ch.send('Failed.'); });
     return;
   }
 });
 
-client.login(BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
